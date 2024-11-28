757007d756fa62fad75c6f57f0bd64203a8a01e4
rachatasomrup <s6504062636276@email.kmutnb.ac.th>
2024-11-28 18:16:38 +0700

    lab scanfv3

--- lab1miinx.c
+++ lab1miinx.c
@@ -3,9 +3,9 @@ int main() {
 	int num1;
 	int num2;
 	printf("n1:");
-	scenf("%d",&num1);
+	scanf("%d",&num1);
 	printf("n2:");
-	scenf("%d",&num2);
+	scanf("%d",&num2);
 	if (num1>num2){
 		printf("num1 is greater than num2");
 		printf("\n%d", (num1-num2));
