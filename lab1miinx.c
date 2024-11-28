69b88b89f20fc85802671f3efe241a8bd61d211a
rachatasomrup <s6504062636276@email.kmutnb.ac.th>
2024-11-28 17:56:59 +0700

    lab1 v1

--- lab1miinx.c
+++ lab1miinx.c
@@ -4,8 +4,8 @@ int main() {
 	int num2=5;
 	if (num1>num2){
 		printf("num1 is greater than num2");
-	}else {
-		printf("num2is greater than num1);
+	}if (num2>num1) {
+		printf("num2is greater than num1");
 	}
 	
 }
