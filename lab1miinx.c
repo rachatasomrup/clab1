1a91d6e3202674fa52909a23dc6cd1eb40bc9fe3
rachatasomrup <s6504062636276@email.kmutnb.ac.th>
2024-11-28 18:14:37 +0700

    lab1 scenfv1

--- lab1miinx.c
+++ lab1miinx.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 int main() {
-	int num1=10;
-	int num2=5;
+	int num1;
+	int num2;
+	printf("n1:");
+	scenf("%d",&num1);
+	printf("n2:");
+	scenf("%d",&num2);
 	if (num1>num2){
 		printf("num1 is greater than num2");
-	}if (num2>num1) {
-		printf("num2is greater than num1");
+		printf("\n%d", (num1-num2));
+	}else {
+		printf("num1 is greater than num2");
+		printf("\n%d", (num2-num1));
 	}
 	
 }
