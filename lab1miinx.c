5c4efe2e92c946c0f61b40f6b669f1c3c8a31ad4
rachatasomrup <s6504062636276@email.kmutnb.ac.th>
2024-11-28 18:29:07 +0700

    lab1 scanfv4 input keybord n1 n2

--- lab1miinx.c
+++ lab1miinx.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 int main() {
-	int num1;
-	int num2;
-	printf("n1:");
-	scanf("%d",&num1);
-	printf("n2:");
-	scanf("%d",&num2);
-	if (num1>num2){
+	int n1,n2;
+	printf("n1:","n2:");
+	scanf("%d",&n1);
+	scanf("%d",&n2);
+	if (n1>n2){
 		printf("num1 is greater than num2");
-		printf("\n%d", (num1-num2));
+		printf("\n%d", (n1-n2));
 	}else {
 		printf("num1 is greater than num2");
-		printf("\n%d", (num2-num1));
+		printf("\n%d", (n2-n1));
 	}
 	
 }
